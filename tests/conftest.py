b7bd89a562e48cdb9302f0fb03b305e8561ea8e0
astrid <f20150082@dubai.bits-pilani.ac.in>
2020-08-23 17:45:16 +0400

    remove blank line

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,6 @@ def user():
 @pytest.fixture(scope='module')
 def test_client():
     flask_app = create_app('flask_test.cfg')
-
     # Flask provides a way to test your application by exposing the Werkzeug test Client
     # and handling the context locals for you.
     testing_client = flask_app.test_client()
