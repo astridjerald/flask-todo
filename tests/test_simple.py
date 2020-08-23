43828f3798f413671b24cd170fd46d45f920da2f
astrid <f20150082@dubai.bits-pilani.ac.in>
2020-08-23 17:48:32 +0400

    remove f

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -16,7 +16,7 @@ def test_simple(n, output):
 
 def test_create_new_user(user):
     user.create({'first_name': 'John', 'last_name': 'Doe', 'email': 'john.doe@gmail.com', 'password': 'root'})
-    query = f'SELECT * FROM User where email="john.doe@gmail.com"'
+    query = 'SELECT * FROM User where email="john.doe@gmail.com"'
     result = user.conn.execute(query).fetchone()
     assert result['first_name'] == 'John'
     assert result['last_name'] == 'Doe'
